44bd85c36d7588b952791427c1a7b254a22b438a
mikaellaofodao <mikaella.nunes@escola.pr.gov.br>
2025-10-23 10:23:57 -0300

    kug9og

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const avanca = document.querySelectorAll('.btn-proximo')
 console.log(avanca)
+
+avanca.forEach(button => {
+ button.addEventListener('click', function(){ 
+ const atual = document.querySelector('.ativo')
+const proximopasso = 'passo-'+ this.getattribute('data-proximo')
+
+atual.classList.remove('ativo')
+document.activeElementbyid(proximopasso).classList.add('ativo')
+
+  }
+})
